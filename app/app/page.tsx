46e07117e963877e0e5d616bc6e6d575d1cd4e37
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-17 22:47:42 +0100

    Feat: Implemented google signup

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-capitalize">App</h1>
+    </div>
+  );
+};
+
+export default App;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import supabase from "@/utils/supabase/client";
 
 export default function Home() {
-  const continueWithGoogle = async () => {
+  const handleContinueWithGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo:"http://localhost:3000/auth/callback"
+      }
     });
   };
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <Button className="flex items-center justify-between gap-4" size={"lg"}>
+      <Button
+        className="flex items-center justify-between gap-4"
+        size={"lg"}
+        onClick={handleContinueWithGoogle}
+      >
         <Image
           src={"/assets/google.svg"}
           width={24}
