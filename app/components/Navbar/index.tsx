8cbcf8295e0ff2ad51333ace79a3e38f84837e1f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-17 16:09:32 +0100

    Feat(ui/components): added navbar components

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+interface NavbarProps {}
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar works!</h1>
+    </div>
+  );
+};
+
+export default Navbar;
