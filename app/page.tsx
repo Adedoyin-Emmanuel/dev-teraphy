708ce87b59cb85b55f8bcb007b4af78077c38477
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-17 21:17:14 +0100

    Feat: Added @supabase package

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl capitalize">hello world</h1>
-
-      <p>my name is adedoyin emmanuel</p>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <Button className="flex items-center justify-between gap-4" size={"lg"}>
+        <Image
+          src={"/assets/google.svg"}
+          width={24}
+          height={24}
+          alt="Google icon"
+        />
+        Continue with google
+      </Button>
     </div>
   );
 }
