bae6dbd4a4874c0eae86e6c146c2560d190b5d3b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-20 00:34:20 +0100

    Feat: Finished appointment page UI

--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import React from "react";
 import Text from "../components/text";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { CalendarIcon, Clock } from "lucide-react";
+import AppointmentCard from "@/app/components/appointment-card";
 
 const Appointments = () => {
   return (
@@ -13,35 +11,30 @@ const Appointments = () => {
       </h1>
       <Text>Pending appointments</Text>
 
-      <section className="appointments my-4">
-        <Card className="max-w-sm rounded overflow-hidden p-4 mb-4">
-          <div className="flex items-center mb-4">
-            <img
-              className="w-12 h-12 rounded-full"
-              src={"https://api.dicebear.com/7.x/big-smile/svg?seed=emmysoft"}
-              alt={`Profile picture of Emma`}
-            />
-            <div className="ml-4">
-              <div className="font-bold text-xl">Adedoyin Emmanuel</div>
-              <div className="text-gray-600 text-sm">Test appointment</div>
-            </div>
-          </div>
-          <div className="flex items-center text-gray-600 text-sm mb-2">
-            <CalendarIcon className="w-4 h-4 mr-1" />
-            2024/06/14
-          </div>
-          <div className="flex items-center text-gray-600 text-sm mb-4">
-            <Clock className="w-4 h-4 mr-1" />
-            12.00OPm
-          </div>
-          <p className="text-gray-700 text-base mb-4">
-            This a test appointment
-          </p>
+      <section className="my-5 grid lg:grid-cols-3">
+        <AppointmentCard
+          title="Test appointment"
+          name="Adedoyin Emmanuel"
+          date="09/14/2005"
+          time="12:00Pm"
+          summary="I just want an appointment please"
+        />
 
-          <Button className="w-full">
-            View details
-          </Button>
-        </Card>
+        <AppointmentCard
+          title="Test appointment"
+          name="Adedoyin Emmysoft"
+          date="09/14/2005"
+          time="12:00Pm"
+          summary="I just want an appointment please"
+        />
+
+        <AppointmentCard
+          title="Test appointment"
+          name="Adedoyin Adeniyi"
+          date="09/14/2005"
+          time="12:00Pm"
+          summary="I just want an appointment please"
+        />
       </section>
     </div>
   );
--- app/components/appointment-card/index.tsx
+++ app/components/appointment-card/index.tsx
@@ -1,19 +1,51 @@
-
-"use client"
 import React from "react";
+import { Card } from "@/components/ui/card";
+import { CalendarIcon, Clock } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-interface Appointment-cardProps {
-    
+interface AppointmentCardProps {
+  className?: string;
+  name: string;
+  title: string;
+  summary: string;
+  date: string;
+  time: string;
 }
 
-const Appointment-card = ()=> {
-
-    return (
-        <div>
-            <h1>Appointment-card works!</h1>
+const AppointmentCard = ({
+  className,
+  name,
+  title,
+  summary,
+  date,
+  time,
+}: AppointmentCardProps) => {
+  return (
+    <Card className={`max-w-sm rounded overflow-hidden p-4 mb-4 ${className}`}>
+      <div className="flex items-center mb-4">
+        <img
+          className="w-12 h-12 rounded-full"
+          src={`https://api.dicebear.com/7.x/big-smile/svg?seed=${name}`}
+          alt={`Profile picture of ${name}`}
+        />
+        <div className="ml-4">
+          <div className="font-bold text-xl">{name}</div>
+          <div className="text-gray-600 text-sm">{title}</div>
         </div>
-    );  
-}
+      </div>
+      <div className="flex items-center text-gray-600 text-sm mb-2">
+        <CalendarIcon className="w-4 h-4 mr-1" />
+        {date}
+      </div>
+      <div className="flex items-center text-gray-600 text-sm mb-4">
+        <Clock className="w-4 h-4 mr-1" />
+        {time}
+      </div>
+      <p className="text-gray-700 text-base mb-4">{summary}</p>
 
-export default Appointment-card;
-    
+      <Button className="w-full">View details</Button>
+    </Card>
+  );
+};
+
+export default AppointmentCard;
