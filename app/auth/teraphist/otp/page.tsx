c7d6b6019c3628692601541cb54bad179815e169
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-20 15:46:37 +0100

    Feat: Added OTP validation

--- app/auth/teraphist/otp/page.tsx
+++ app/auth/teraphist/otp/page.tsx
@@ -5,20 +5,33 @@ import {
   InputOTP,
   InputOTPGroup,
   InputOTPSlot,
-  InputOTPSeparator,
 } from "@/components/ui/input-otp";
 import { Button } from "@/components/ui/button";
 import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+import { useToast } from "@/components/ui/use-toast";
 
 const OTP = () => {
   const [value, setValue] = React.useState<string>("");
+  const { toast } = useToast();
+
+  const handleSubmit = async () => {
+    if (!value || value.length !== 6) {
+      toast({
+        title: "Error",
+        description: "OTP is required",
+        variant: "destructive",
+        duration: 3000,
+      });
+      return;
+    }
+  };
 
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <section className="w-11/12 md:w-1/2 xl:w-1/4">
         <section className="header-section my-8">
           <h3 className="text-3xl capitalize font-bold">OTP</h3>
-          <p>Input the OTP you sent to your email </p>
+          <Text>Input the OTP you sent to your email </Text>
         </section>
 
         <InputOTP
@@ -37,7 +50,9 @@ const OTP = () => {
           </InputOTPGroup>
         </InputOTP>
 
-        <Button className="w-full my-3">Proceed</Button>
+        <Button className="w-full my-3" onClick={handleSubmit}>
+          Proceed
+        </Button>
       </section>
     </div>
   );
