e85b41daae0112f8d80aaac23521f963f7fb7359
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-20 16:27:56 +0100

    Feat: Finshed login

--- app/auth/teraphist/login/page.tsx
+++ app/auth/teraphist/login/page.tsx
@@ -1,13 +1,68 @@
+"use client";
+import React from "react";
 import Text from "@/app/components/text";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Axios } from "@/app/api/axios";
+import { useToast } from "@/components/ui/use-toast";
+import Loader from "@/app/components/loader";
 
 const TeraphistLogin = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [formData, setFormData] = React.useState({
+    email: "",
+    password: "",
+  });
+  const { toast } = useToast();
+
+  const handleInputChange = (e: React.FormEvent<HTMLFormElement> | any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement> | any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const { email, password } = formData;
+    if (!email || !password) {
+      toast({
+        title: "Validation Error",
+        description: "All fields are required",
+        duration: 3000,
+      });
+      return;
+    }
+    try {
+      const { data } = await Axios.post("/auth/login/teraphist", {
+        email,
+        password,
+      });
+
+      toast({
+        title: "Login successful",
+        description: data.message,
+      });
+
+      router.push("/dashboard");
+    } catch (error: any) {
+      toast({
+        title: "Error",
+        description: error.response.data.message || error.message,
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="w-screen h-screen flex items-center flex-col justify-around">
-      <form className="w-11/12 md:w-1/2 xl:w-1/4">
+      {isLoading && <Loader loading={isLoading} />}
+      <form className="w-11/12 md:w-1/2 xl:w-1/4" onSubmit={handleSubmit}>
         <section className="header-section my-8">
           <h3 className="text-3xl capitalize font-bold">Login</h3>
           <p>Welcome back </p>
@@ -15,7 +70,13 @@ const TeraphistLogin = () => {
 
         <section className="my-4 mb-5">
           <Label htmlFor="email">Email</Label>
-          <Input type="email" name="email" placeholder="Enter your email" />
+          <Input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
         </section>
 
         <section className="my-4 mb-5">
@@ -26,6 +87,8 @@ const TeraphistLogin = () => {
             type="password"
             name="password"
             placeholder="Shhh it's a secret"
+            value={formData.password}
+            onChange={handleInputChange}
           />
         </section>
 
--- app/auth/teraphist/otp/page.tsx
+++ app/auth/teraphist/otp/page.tsx
@@ -9,12 +9,18 @@ import {
 import { Button } from "@/components/ui/button";
 import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
 import { useToast } from "@/components/ui/use-toast";
+import { Axios } from "@/app/api/axios";
+import Loader from "@/app/components/loader";
+import { useRouter } from "next/navigation";
 
 const OTP = () => {
   const [value, setValue] = React.useState<string>("");
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const { toast } = useToast();
+  const router = useRouter();
 
   const handleSubmit = async () => {
+    setIsLoading(true);
     if (!value || value.length !== 6) {
       toast({
         title: "Error",
@@ -22,12 +28,35 @@ const OTP = () => {
         variant: "destructive",
         duration: 3000,
       });
+      setIsLoading(false);
       return;
+    }
+
+    try {
+      const { data } = await Axios.post("/auth/verify-otp", {
+        otp: value,
+      });
+
+      toast({
+        title: "Verification successful",
+        description: data.message,
+      });
+
+      router.push("/auth/teraphist/login");
+    } catch (error: any) {
+      toast({
+        title: "Error",
+        description: error.response.data.message || error.message,
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
+      {isLoading && <Loader loading={isLoading} />}
       <section className="w-11/12 md:w-1/2 xl:w-1/4">
         <section className="header-section my-8">
           <h3 className="text-3xl capitalize font-bold">OTP</h3>
