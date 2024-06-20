87015b2bcda6be192c742a98d53331733edd4902
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-20 08:35:27 +0100

    Feat: Added settings page

---
+++ app/components/settings/index.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from "react";
+
+interface SettingsProps {
+    
+}
+
+const Settings = ()=> {
+
+    return (
+        <div>
+            <h1>Settings works!</h1>
+        </div>
+    );  
+}
+
+export default Settings;
+    
